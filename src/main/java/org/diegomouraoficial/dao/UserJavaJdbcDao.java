9e4882348bfc2ec0c6212e3ec70f4918b558b6ad
Diego Moura <programmercodeoficial@gmail.com>
2023-10-03 08:05:49 -0300

    criando a logica do insert no banco de daods manual

--- src/main/java/org/diegomouraoficial/dao/UserJavaJdbcDao.java
+++ src/main/java/org/diegomouraoficial/dao/UserJavaJdbcDao.java
@@ -1,13 +1,36 @@
 package org.diegomouraoficial.dao;
 
 import conexaojdbc.SingleConnection;
-
+import org.diegomouraoficial.model.UserJavaJdbc;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class UserJavaJdbcDao {
 
-    public UserJavaJdbcDao() {
-        Connection connection = SingleConnection.getConnection();
+    private final Connection connection;
+
+    public UserJavaJdbcDao(Connection connection) {
+        this.connection = connection;
     }
+
+    public void salvar(UserJavaJdbc userJavaJdbc) {
+
+        try {
+            String sql = "insert into userjavajdbc (id, nome, email) values (?, ?, ?)";
+            PreparedStatement insert = connection.prepareStatement(sql);
+            insert.setLong(1, 3);
+            insert.setString(2, "Lupi salvar DAO");
+            insert.setString(3, "lupi@teste.com.br");
+            insert.execute();
+            connection.commit(); // Salva no banco de dados
+        } catch (Exception e) {
+            logger.log(Level.WARNING,"Erro ao tentar Inserir no banco de dados", e);
+        }
+    }
+
+    private static final Logger logger = Logger.getLogger(UserJavaJdbcDao.class.getName());
 
 }
--- src/test/java/conexaojdbc/SingleConnectionTest.java
+++ src/test/java/conexaojdbc/SingleConnectionTest.java
@@ -1,10 +1,20 @@
 package conexaojdbc;
 
+import org.diegomouraoficial.dao.UserJavaJdbcDao;
+import org.diegomouraoficial.model.UserJavaJdbc;
 import org.junit.Test;
+
+import java.sql.Connection;
 
 public class SingleConnectionTest {
     @Test
     public void initBanco() {
-        SingleConnection.getConnection();
+
+        Connection connection = SingleConnection.getConnection();
+
+        UserJavaJdbcDao userJavaJdbcDao = new UserJavaJdbcDao(connection);
+        UserJavaJdbc userJavaJdbc = new UserJavaJdbc();
+
+        userJavaJdbcDao.salvar(userJavaJdbc);
     }
 }
